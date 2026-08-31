1149a8e0eeaa00cb1148be882b088254bae9069c
HalfBaked991 <Halfbakedideaslab0@gmail.com>
2026-08-31 15:51:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ window.HB_CONFIG = {
   EMAILJS_PUBLIC_KEY: "8yPkduM1pQMGrS_wR",
   EMAILJS_SERVICE_ID: "service_588z9pc",
   EMAILJS_TEMPLATE_ID: "template_fwmif7p",
-  EMAILJS_REVIEW_TEMPLATE_ID: "YOUR_REVIEW_TEMPLATE_ID",
+  EMAILJS_REVIEW_TEMPLATE_ID: "template_14ko9ev",
   EMAILJS_CONTACT_TEMPLATE_ID: "YOUR_CONTACT_TEMPLATE_ID",
 
   // Add your actual donation destinations when ready.
